ed3c3944f29965ee8c0cb04b459a68546c55960f
psyduck007 <psyduck007@outlook.com>
2020-05-25 14:08:12 +0800

    first

---
+++ Models/TableModelMisc.cs
@@ -0,0 +1,31 @@
+﻿// ////////////////////////////////////////////////////
+// StartTime:      2020/5/25 11:24
+// FileName:       TableModelMisc.cs
+// Author:           psyduck007@outlook.com
+// Purpose:         表模型其他
+// TODO:
+// ////////////////////////////////////////////////////
+//
+//
+using System;
+namespace DBACommonPackage.Models
+{
+    public abstract class ModelHistory
+    {
+        public string DeleteBy { get; set; } = "";
+        public DateTime DeleteTime { get; set; } = DateTime.Now;
+    }
+
+    public abstract class ModelFootPrintFields
+    {
+        public string CreateBy { get; set; }
+        public string UpdateBy { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+    }
+
+    public abstract class ModelEnvironment
+    {
+        public string Environment { get; set; }
+    }
+}
---
+++ Utils/HostUtil.cs
@@ -0,0 +1,38 @@
+﻿// ////////////////////////////////////////////////////
+// StartTime:      2020/5/25 11:26
+// FileName:       HostUtil.cs
+// Author:           psyduck007@outlook.com
+// Purpose:         本机相关功能
+// TODO:
+// ////////////////////////////////////////////////////
+//
+//
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DBACommonPackage.Utils
+{
+    public static class HostUtil
+    {
+        /// <summary>
+        /// 获取主机名
+        /// </summary>
+        public static string LocalHostname
+        {
+            get { return Dns.GetHostName(); }
+        }
+
+        /// <summary>
+        /// 获取本机IP
+        /// </summary>
+        public static string LocalIP
+        {
+            get
+            {
+                var bomList = Dns.GetHostAddresses(Dns.GetHostName());
+                return bomList.FirstOrDefault(ha => ha.AddressFamily == AddressFamily.InterNetwork).ToString();
+            }
+        }
+    }
+}
