60732b270ed580ad0cc2479af548bc7ac9c8a870
psyduck007 <psyduck007@outlook.com>
2020-06-18 16:03:12 +0800

    0.2.10

---
+++ Models/Setting.cs
@@ -0,0 +1,22 @@
+﻿// ////////////////////////////////////////////////////
+// StartTime:      2020/6/18 16:00
+// FileName:       Setting.cs
+// Author:           psyduck007@outlook.com
+// Purpose:         Lazy dog does not write purpose
+// TODO:
+// ////////////////////////////////////////////////////
+//
+//
+using System;
+namespace DBACommonPackage.Models
+{
+    public class HostPort
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+    public class HTTPSimpleAPI : HostPort
+    {
+        public string APIPath { get; set; }
+    }
+}
--- Utils/JsonUtil.cs
+++ Utils/JsonUtil.cs
@@ -9,6 +9,7 @@
 //
 using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Text.Unicode;
 
 namespace DBACommonPackage.Utils
