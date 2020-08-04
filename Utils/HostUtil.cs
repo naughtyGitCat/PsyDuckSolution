d6d08ea9eeaee5f508e8a1f82d70675dc9374036
psyduck007 <psyduck007@outlook.com>
2020-08-04 21:29:16 +0800

    * DBACommonPackage.csproj: add version
    * HostUtil.cs: fix, add obsolete annotation to hostutil static class

--- Utils/HostUtil.cs
+++ Utils/HostUtil.cs
@@ -7,6 +7,7 @@
 // ////////////////////////////////////////////////////
 //
 //
+using System;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -14,6 +15,7 @@ using DBACommonPackage.Models;
 
 namespace DBACommonPackage.Utils
 {
+    [Obsolete("Deprecated, use HostInfoFactory instead of it", error:false)]
     public static class HostUtil
     {
         /// <summary>
@@ -35,59 +37,70 @@ namespace DBACommonPackage.Utils
                 return bomList.FirstOrDefault(ha => ha.AddressFamily == AddressFamily.InterNetwork).ToString();
             }
         }
-
-        public static class HostInfoFactory
+    }
+    public static class HostInfoFactory
+    {
+        /// <summary>
+        /// like darwin/linux/windows
+        /// </summary>
+        /// <returns></returns>
+        public static string ReadOSType()
         {
-            /// <summary>
-            /// like darwin/linux/windows
-            /// </summary>
-            /// <returns></returns>
-            public static string ReadOSType()
+            var rawString = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            return rawString.Split(" ")[0].ToLower();
+        }
+        /// <summary>
+        /// use dns,in danger of cache
+        /// </summary>
+        /// <returns></returns>
+        public static string GetLocalIPByDNS()
+        {
+            var bomList = Dns.GetHostAddresses(Dns.GetHostName());
+            return bomList.FirstOrDefault(ha => ha.AddressFamily == AddressFamily.InterNetwork).ToString();
+        }
+        /// <summary>
+        /// use socket
+        /// </summary>
+        /// <returns></returns>
+        public static string GetLocalIPBySocket()
+        {
+            string ip = "127.0.0.1";
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
             {
-                var rawString = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
-                return rawString.Split(" ")[0].ToLower();
+                IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                sock.Bind(localEndPoint);
+                localEndPoint = (IPEndPoint)sock.LocalEndPoint;
+                ip = localEndPoint.Address.ToString();
             }
-            /// <summary>
-            /// use dns,in danger of cache
-            /// </summary>
-            /// <returns></returns>
-            public static string GetLocalIPByDNS()
+            finally
             {
-                var bomList = Dns.GetHostAddresses(Dns.GetHostName());
-                return bomList.FirstOrDefault(ha => ha.AddressFamily == AddressFamily.InterNetwork).ToString();
+                sock.Close();
             }
-            /// <summary>
-            /// use socket
-            /// </summary>
-            /// <returns></returns>
-            public static string GetLocalIPBySocket()
-            {
-                string ip = "127.0.0.1";
-                Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                try
-                {
-                    IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 0);
-                    sock.Bind(localEndPoint);
-                    localEndPoint = (IPEndPoint)sock.LocalEndPoint;
-                    ip = localEndPoint.Address.ToString();
-                }
-                finally
-                {
-                    sock.Close();
-                }
-                return ip;
-            }
+            return ip;
+        }
 
-            public static HostInfo FetchHostInfo(bool useSocket=false)
+        public static HostInfo FetchHostInfo(bool useSocket = false)
+        {
+            return new HostInfo
             {
-                return new HostInfo
-                {
-                    OSType = ReadOSType(),
-                    HostIP = useSocket ? GetLocalIPBySocket() : GetLocalIPByDNS(),
-                    HostName = Dns.GetHostName()
-                };
-            }
-
+                OSType = ReadOSType(),
+                HostIP = useSocket ? GetLocalIPBySocket() : GetLocalIPByDNS(),
+                HostName = Dns.GetHostName()
+            };
+        }
+        /// <summary>
+        /// find free port with no lock
+        /// the port might be occupied when u really start to use it
+        /// </summary>
+        /// <returns></returns>
+        public static int FindFreePort()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+            return port;
         }
     }
 }
